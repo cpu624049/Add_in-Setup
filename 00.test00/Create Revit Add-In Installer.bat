edb8012ce7dec3f2a07b0e586f44dbd28f2116af
cpu624049 <kjs940426@naver.com>
2025-04-18 18:06:41 +0900

    temporary save

---
+++ 00.test00/Create Revit Add-In Installer.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d "D:\01.업무\3.업무내용\01.진행중\01.VBG R&D\02.파일\04.테스트\00.test00"
+echo 🔁 Revit Add-in Installer Test.py 실행 중...
+"c:\Users\Linetek\AppData\Local\Programs\Python\Python313\python.exe" "Revit Add-in Installer Test.py"
